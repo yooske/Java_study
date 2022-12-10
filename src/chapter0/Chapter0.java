196b2cfb91b50433f084eaab1b2b675a9523c86b
yooske <yooske0214@gmail.com>
2022-12-10 15:48:49 +0000

    [add] Java_study chapter0

---
+++ src/chapter0/Chapter0.java
@@ -0,0 +1,14 @@
+package chapter0;
+
+public class Chapter0 {
+	public static void main(String[] args) {
+		System.out.println("さきやま");
+		System.out.println("26歳です。");
+		System.out.println("動物が好きです。");
+		System.out.println("31 + 31 の計算をします。");
+		System.out.println(31 + 31);
+		int x;
+		x = 6;
+		System.out.println(x * x * 3.14);
+	}
+}
