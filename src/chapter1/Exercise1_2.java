93f84be5be37f821dc5bff0c342c9cea48ce0dd2
yooske <yooske0214@gmail.com>
2022-12-11 15:42:02 +0000

    [add] JavaStudy chapter1 finish

---
+++ src/chapter1/Exercise1_2.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+public class Exercise1_2 {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 5;
+		int c = a * b;
+		System.out.println("縦幅 " + a + " 横幅 " + b + " の長方形の面積は、" + c);
+	}
+}
---
+++ src/chapter1/Exercise1_3.java
@@ -0,0 +1,11 @@
+package chapter1;
+
+public class Exercise1_3 {
+	public static void main(String[] args) {
+		boolean error = true;
+		char car = '駆';
+		final double PI = 3.14;
+		long pi2 = 314159265853979L;
+		String rpg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+	}
+}
