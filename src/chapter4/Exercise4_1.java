fc0954a3a4d4316823c7769f32405ea1d6f4ec05
yooske <yooske0214@gmail.com>
2022-12-22 03:07:34 +0000

    [add] JavaStudy chapter4 finish

---
+++ src/chapter4/Exercise4_1.java
@@ -0,0 +1,10 @@
+package chapter4;
+
+public class Exercise4_1 {
+	public static void main(String[] args) {
+		int[] points = new int[4];
+		double[] weights = new double[5];
+		boolean[] answers = new boolean[3];
+		String[] names = new String[3];
+	}
+}
