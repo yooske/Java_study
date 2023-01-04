ce3151627a5060710d19a75fcaccc177a4cab051
yooske <yooske0214@gmail.com>
2023-01-04 09:18:10 +0000

    [add] JavaStudy chapter8.2 finish

---
+++ src/chapter8/Code8_Main.java
@@ -0,0 +1,7 @@
+package chapter8;
+
+public class Code8_Main {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/chapter8/Code8_Matango.java
@@ -0,0 +1,6 @@
+package chapter8;
+
+public class Code8_Matango {
+	int hp;
+	final int LEVEL = 10;
+}
